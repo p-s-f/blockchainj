fc201c4391e2be06ff743aa7314bc81ec04ac9bc
michael.beddow <michael.beddow@wandisco.com>
2017-09-07 16:42:14 +0100

    fix total time

--- src/main/java/com/kbtech/blockchain/BlockchainMain.java
+++ src/main/java/com/kbtech/blockchain/BlockchainMain.java
@@ -20,16 +20,15 @@ public class BlockchainMain {
     StopWatch stopWatch = new StopWatch();
     stopWatch.start();
 
-    long startTime = stopWatch.getStartTime();
     long currentTime = stopWatch.getTime();
 
-    while (!isValidHashDifficulty(hash, 8, false)) {
+    while (!isValidHashDifficulty(hash, 6, false)) {
       nonce ++;
       msg = String.format("%s%s", input, nonce);
       hash = hashToSHA256(msg);
       attempt ++;
       if (attempt%1000000 == 0) {
-        million++;
+        million ++;
         long timeDiff = (stopWatch.getTime() - currentTime) / 1000;
         System.out.println(String.format("%s million hashes! took [%ss]", million, timeDiff));
         currentTime = stopWatch.getTime();
@@ -37,9 +36,11 @@ public class BlockchainMain {
 //      System.out.println(String.format("Input: %s   - Hash: %s", msg, hash));
     }
 
+    stopWatch.split();
+    System.out.println(String.format("got it! - took [%ss]", (stopWatch.getSplitTime()) / 1000));
+
     stopWatch.stop();
 
-    System.out.println(String.format("got it! - took [%ss]", (stopWatch.getTime() - startTime) / 1000));
     hash = hashToSHA256(msg);
     System.out.println(String.format("Input: %s   - Hash: %s", msg, hash));
 
